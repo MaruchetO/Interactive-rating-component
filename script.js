f44fc8f08989fd5d48e90cd47127fc6e6d9d43b3
marucheto <maruchet2@gmail.com>
2025-04-04 17:00:39 +0700

    Make button interactive

---
+++ script.js
@@ -0,0 +1,38 @@
+const modal = document.querySelector(".modal");
+const buttons = document.querySelectorAll(".btn");
+const star = document.querySelector(".summary-star");
+
+function selectedButton(selector) {
+  const button = document.querySelector(selector);
+  if (!button.classList.contains("selected")) {
+    turnOffPreviousButton();
+    button.classList.add("selected");
+  }
+}
+
+function turnOffPreviousButton() {
+  const selected = document.querySelector(".selected");
+  if (selected) {
+    selected.classList.remove("selected");
+  }
+}
+
+function showModal() {
+  const selected = document.querySelector(".selected");
+  if (selected) {
+    modal.style.display = "flex";
+  } else {
+    alert('Please rate us before submit.')
+  }
+}
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+     star.innerText = button.value;
+  });
+});
